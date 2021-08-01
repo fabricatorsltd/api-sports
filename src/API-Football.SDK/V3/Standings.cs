0eef929eae9acbcfd32821b2dfe59ca3601bb97b
Pietro di Caprio <pietro@fabricators.ltd>
2021-08-01 17:13:33 +0200

    Create Standings calls

--- src/API-Football.SDK/V3/Standings.cs
+++ src/API-Football.SDK/V3/Standings.cs
@@ -4,9 +4,27 @@ namespace API_Football.SDK.V3
 {
     public static class Standings
     {
-        public static ApiResponse<List<Models.Standing>> GetStandings(this Instance instance)
+        public static ApiResponse<List<Models.Standing>> GetStandingsByLeague(
+            this Instance instance,
+            ushort season,
+            ushort league)
         {
-            return instance.DoCall<List<Models.Standing>>("standings");
+            return instance.DoCall<List<Models.Standing>>($"standings?season={season}&league={league}");
+        }
+        public static ApiResponse<List<Models.Standing>> GetStandingsByTeam(
+            this Instance instance,
+            ushort season,
+            ushort team)
+        {
+            return instance.DoCall<List<Models.Standing>>($"standings?season={season}&team={team}");
+        }
+        public static ApiResponse<List<Models.Standing>> GetStandings(
+            this Instance instance,
+            ushort season,
+            ushort league,
+            ushort team)
+        {
+            return instance.DoCall<List<Models.Standing>>($"standings?season={season}&league={league}&team={team}");
         }
     }
 }
