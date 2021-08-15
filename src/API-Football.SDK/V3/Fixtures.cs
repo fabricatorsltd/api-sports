fb587aa91d841501bf0509d85511fe96e193dc17
Pietro di Caprio <pietro@fabricators.ltd>
2021-08-15 14:00:56 +0200

    Allow to get fixtures by league and season

--- src/API-Football.SDK/V3/Fixtures.cs
+++ src/API-Football.SDK/V3/Fixtures.cs
@@ -10,6 +10,14 @@ namespace API_Football.SDK.V3
         {
             return instance.DoCall<List<Models.ApiFixtureResponse>>($"fixtures?live={matches}");
         }
+        
+        public static ApiResponse<List<Models.ApiFixtureResponse>> GetFixtures(
+            this Instance instance,
+            ushort leagueId,
+            ushort season)
+        {
+            return instance.DoCall<List<Models.ApiFixtureResponse>>($"fixtures?league={leagueId}&season={season}");
+        }
 
         public static ApiResponse<List<Models.ApiFixtureResponse>> GetFixture(
             this Instance instance,
