e38de937ec0e359f8b26334f878aec18e61fc99d
Pietro di Caprio <pietro@fabricators.ltd>
2026-01-04 14:34:57 +0100

    add Formula 1 sample responses and update unit tests

--- tests/ApiSports.Sdk.Formula1.Tests/Helpers/SamplePaths.cs
+++ tests/ApiSports.Sdk.Formula1.Tests/Helpers/SamplePaths.cs
@@ -2,9 +2,9 @@ namespace ApiSports.Sdk.Formula1.Tests.Helpers;
 
 public static class SamplePaths
 {
-    public static string Formula1Root { get; } = Path.Combine(AppContext.BaseDirectory, "samples", "formula1");
+    public static string Formula1Root { get; } = Path.Combine(AppContext.BaseDirectory, "samples");
 
-    public static string OpenApiPath { get; } = Path.Combine(Formula1Root, "openapi.yaml");
+    public static string OpenApiPath { get; } = Path.Combine(Formula1Root, "formula1-v1.yaml");
 
     public static string ResolveFormula1Path(string relativePath)
     {
